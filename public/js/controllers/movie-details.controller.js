96eeed4d673d7f3eebf98ed8db7b71b0c0c708ce
raleigh wayland <rtwayland@gmail.com>
2017-03-27 22:12:41 -0600

    Utilizing sessionStorage for speed

--- public/js/controllers/movie-details.controller.js
+++ public/js/controllers/movie-details.controller.js
@@ -1,18 +1,46 @@
 angular.module('app')
     .controller('MovieDetailsController', function($scope, $state, GuideboxService) {
-        GuideboxService.getMovieById($state.params.id)
-            .then(function(res) {
-                $scope.movie = res;
-                $scope.movie.cast = res.cast.join(', ');
-                let duration = Number(res.duration);
+        if (sessionStorage.masterMovie && sessionStorage.masterMovieList) {
 
-                var hours = Math.floor(duration / 3600);
-                var minutes = Math.floor(duration % 3600 / 60);
+            $scope.masterMovie = angular.fromJson(sessionStorage.masterMovie);
+            $scope.masterMovieList = angular.fromJson(sessionStorage.masterMovieList);
 
-                $scope.movie.duration = `${hours} hrs ${minutes} min`;
+            if ($scope.masterMovie.id == $state.params.id) {
+                $scope.movie = $scope.masterMovie;
+                $scope.movie.cast = $scope.masterMovie.cast.join(', ');
+                $scope.movie.duration = getConvertedTime($scope.masterMovie.duration);
+            } else {
+                for (var i = 0; i < $scope.masterMovieList.length; i++) {
+                    if ($scope.masterMovieList[i].id == $state.params.id) {
+                        $scope.movie = $scope.masterMovieList[i];
+                        $scope.movie.cast = $scope.masterMovieList[i].cast.join(', ');
+                        $scope.movie.duration = getConvertedTime($scope.masterMovieList[i].duration);
+                        break;
+                    }
+                }
+            }
+        } else {
+            GuideboxService.getMovieById($state.params.id)
+                .then(function(res) {
+                    $scope.movie = res;
+                    $scope.movie.cast = res.cast.join(', ');
 
-                console.log($scope.movie);
-            }, function(err) {
-                console.log(err);
-            })
+                    $scope.movie.duration = getConvertedTime(res.duration);
+
+                }, function(err) {
+                    console.log(err);
+                })
+
+        }
+
+
+        function getConvertedTime(time) {
+            let duration = Number(time);
+
+            var hours = Math.floor(duration / 3600);
+            var minutes = Math.floor(duration % 3600 / 60);
+
+            return `${hours} hrs ${minutes} min`;
+        }
+
     });
