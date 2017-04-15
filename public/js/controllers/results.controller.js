35c118bbfa30542173750dd50febbf78f0710f0f
raleigh wayland <rtwayland@gmail.com>
2017-04-15 00:23:11 -0600

    Modularizing code. More readable and organized

--- public/js/controllers/results.controller.js
+++ public/js/controllers/results.controller.js
@@ -63,67 +63,82 @@ angular.module('app')
         /***************** Filter Results *****************/
         $scope.filterResults = function() {
             let masterList = $scope.masterMovieList;
-            let subscriptionFiltered = [];
-            let filtered = [];
+            // Subscription Variables
             let netflix = $scope.subscriptions.netflix;
             let prime = $scope.subscriptions.prime;
             let hulu = $scope.subscriptions.hulu;
+            // Rating Variables
+            let r = $scope.ratings.r;
+            let pg13 = $scope.ratings.pg13;
+            let pg = $scope.ratings.pg;
+            let g = $scope.ratings.g;
+            let nr = $scope.ratings.nr;
+            let nc = $scope.ratings.nc;
 
             if (netflix || prime || hulu) {
-                for (var i = 0; i < masterList.length; i++) {
-                    if (masterList[i].subscription_web_sources.length) {
-                        let subscriptionList = masterList[i].subscription_web_sources;
-                        let inserted = false;
-                        for (var j = 0; j < subscriptionList.length && !inserted; j++) {
-                            switch (subscriptionList[j].display_name) {
-                                case "Netflix":
-                                    if (netflix) {
-                                        subscriptionFiltered.push(masterList[i]);
-                                        inserted = true;
-                                    }
-                                    break;
-                                case "Amazon Prime":
-                                    if (prime) {
-                                        subscriptionFiltered.push(masterList[i]);
-                                        inserted = true;
-                                    }
-                                    break;
-                                case "Hulu":
-                                    if (hulu) {
-                                        subscriptionFiltered.push(masterList[i]);
-                                        inserted = true;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
+                masterList = filterBySubscription(masterList, $scope.subscriptions);
+            }
+
+            if (!r || !pg13 || !pg || !g || !nr || !nc) {
+                masterList = filterByRating(masterList, $scope.ratings);
+            }
+
+            $scope.displayList = masterList;
+        }
+
+        function filterBySubscription(masterList, subscriptions) {
+            let results = [];
+            for (var i = 0; i < masterList.length; i++) {
+                if (masterList[i].subscription_web_sources.length) {
+                    let subscriptionList = masterList[i].subscription_web_sources;
+                    let inserted = false;
+                    for (var j = 0; j < subscriptionList.length && !inserted; j++) {
+                        switch (subscriptionList[j].display_name) {
+                            case "Netflix":
+                                if (subscriptions.netflix) {
+                                    results.push(masterList[i]);
+                                    inserted = true;
+                                }
+                                break;
+                            case "Amazon Prime":
+                                if (subscriptions.prime) {
+                                    results.push(masterList[i]);
+                                    inserted = true;
+                                }
+                                break;
+                            case "Hulu":
+                                if (subscriptions.hulu) {
+                                    results.push(masterList[i]);
+                                    inserted = true;
+                                }
+                                break;
+                            default:
+                                break;
                         }
                     }
                 }
             }
+            return results;
+        }
 
-
-            if (subscriptionFiltered.length || (netflix || prime || hulu)) {
-                masterList = subscriptionFiltered;
-            }
-
+        function filterByRating(masterList, ratings) {
+            let results = [];
             for (var i = 0; i < masterList.length; i++) {
-                if (masterList[i].rating == 'R' && $scope.ratings.r == true) {
-                    filtered.push(masterList[i]);
-                } else if (masterList[i].rating == 'PG-13' && $scope.ratings.pg13 == true) {
-                    filtered.push(masterList[i]);
-                } else if (masterList[i].rating == 'PG' && $scope.ratings.pg == true) {
-                    filtered.push(masterList[i]);
-                } else if (masterList[i].rating == 'G' && $scope.ratings.g == true) {
-                    filtered.push(masterList[i]);
-                } else if (masterList[i].rating == 'NR' && $scope.ratings.nr == true) {
-                    filtered.push(masterList[i]);
-                } else if (masterList[i].rating == 'NC-17' && $scope.ratings.nc == true) {
-                    filtered.push(masterList[i]);
+                if (masterList[i].rating == 'R' && ratings.r == true) {
+                    results.push(masterList[i]);
+                } else if (masterList[i].rating == 'PG-13' && ratings.pg13 == true) {
+                    results.push(masterList[i]);
+                } else if (masterList[i].rating == 'PG' && ratings.pg == true) {
+                    results.push(masterList[i]);
+                } else if (masterList[i].rating == 'G' && ratings.g == true) {
+                    results.push(masterList[i]);
+                } else if (masterList[i].rating == 'NR' && ratings.nr == true) {
+                    results.push(masterList[i]);
+                } else if (masterList[i].rating == 'NC-17' && ratings.nc == true) {
+                    results.push(masterList[i]);
                 }
             }
-
-            $scope.displayList = filtered;
+            return results;
         }
 
         $scope.clearFilters = function() {
