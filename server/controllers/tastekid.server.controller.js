2d741171ca9a955faf717bee97b437a0e9015179
raleigh wayland <rtwayland@gmail.com>
2017-04-15 00:19:59 -0600

    Refactoring api call to use request module instead of http

--- server/controllers/tastekid.server.controller.js
+++ server/controllers/tastekid.server.controller.js
@@ -1,28 +1,23 @@
-const http = require('http');
+const request = require('request');
 module.exports = {
     getSuggestions(req, res) {
-        let limit = 20;
-        let search = encodeURI(req.params.search);
-        let apiPath = '/api/similar?k=228946-moviegur-14IA4LWB&q=' + search + '&type=movies&limit=' + limit;
+      let limit = 20;
+      let search = encodeURI(req.params.search);
+      let apiPath = 'https://tastedive.com/api/similar?k=228946-moviegur-14IA4LWB&q=' + search + '&type=movies&limit=' + limit;
         var options = {
-            host: 'www.tastekid.com',
-            port: 80,
-            path: apiPath,
-            method: 'GET'
+            uri: apiPath,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
         };
-
-        http.request(options).on('response', function(response) {
-            var data = '';
-            response.on("data", function(chunk) {
-                data += chunk;
-            });
-            response.on('end', function() {
-                var results = JSON.parse(data);
-                if (!results.Similar.Results.length) {
-                    return res.status(404).send('No Results');
+        request(options,
+            function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    return res.status(200).json(body.Similar.Results);
+                } else {
+                    return res.status(404).json(error);
                 }
-                return res.status(200).json(results.Similar.Results);
             });
-        }).end();
     }
 }
