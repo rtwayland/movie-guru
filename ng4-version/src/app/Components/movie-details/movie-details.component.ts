beb46dfb0a379e43798cd7f0ca4f6c39386682d7
Raleigh Wayland <rtwayland@gmail.com>
2017-05-23 20:36:26 -0600

    Getting movie details page up and running

--- ng4-version/src/app/Components/movie-details/movie-details.component.ts
+++ ng4-version/src/app/Components/movie-details/movie-details.component.ts
@@ -4,65 +4,69 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { GuideboxService } from '../../Services/guidebox.service';
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.scss']
+	selector: 'app-movie-details',
+	templateUrl: './movie-details.component.html',
+	styleUrls: ['./movie-details.component.scss']
 })
 export class MovieDetailsComponent implements OnInit {
     movieId: number;
+    masterMovie: any;
+	masterMovieList: any[];
+    movie: any;
 
-  constructor(private guideboxService: GuideboxService, private route: ActivatedRoute) { }
+	constructor(private guideboxService: GuideboxService, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-      this.movieId = this.route.snapshot.params['id'];
-  }
-  /*
-  if (sessionStorage.masterMovie && sessionStorage.masterMovieList) {
+	ngOnInit() {
+		this.movieId = this.route.snapshot.params['id'];
+		if (sessionStorage.masterMovie && sessionStorage.masterMovieList) {
 
-      $scope.masterMovie = angular.fromJson(sessionStorage.masterMovie);
-      $scope.masterMovieList = angular.fromJson(sessionStorage.masterMovieList);
+			this.masterMovie = JSON.parse(sessionStorage.masterMovie);
+			this.masterMovieList = JSON.parse(sessionStorage.masterMovieList);
 
-      if ($scope.masterMovie.id == $state.params.id) {
-          $scope.movie = $scope.masterMovie;
-          $scope.movie.cast = $scope.masterMovie.cast.join(', ');
-          $scope.movie.duration = getConvertedTime($scope.masterMovie.duration);
-      } else {
-          for (var i = 0; i < $scope.masterMovieList.length; i++) {
-              if ($scope.masterMovieList[i].id == $state.params.id) {
-                  $scope.movie = $scope.masterMovieList[i];
-                  $scope.movie.cast = $scope.masterMovieList[i].cast.join(', ');
-                  $scope.movie.duration = getConvertedTime($scope.masterMovieList[i].duration);
-                  break;
-              }
-          }
-      }
-  } else {
-      GuideboxService.getMovieById($state.params.id)
-          .then(function(res) {
-              $scope.movie = res;
-              $scope.movie.cast = res.cast.join(', ');
+			if (this.masterMovie.id == this.movieId) {
+				this.movie = this.masterMovie;
+				this.movie.cast = this.masterMovie.cast.join(', ');
+				this.movie.duration = this.getConvertedTime(this.masterMovie.duration);
+			} else {
+				for (var i = 0; i < this.masterMovieList.length; i++) {
+					if (this.masterMovieList[i].id == this.movieId) {
+						this.movie = this.masterMovieList[i];
+						this.movie.cast = this.masterMovieList[i].cast.join(', ');
+						this.movie.duration = this.getConvertedTime(this.masterMovieList[i].duration);
+						break;
+					}
+				}
+			}
+		} else {
+			this.guideboxService.getMovieById(this.movieId)
+				.subscribe(
+				(res) => {
+					this.movie = res;
+					this.movie.cast = res.cast.join(', ');
 
-              $scope.movie.duration = getConvertedTime(res.duration);
+					this.movie.duration = this.getConvertedTime(res.duration);
 
-          }, function(err) {
-              console.log(err);
-          })
+				}, (err) => {
+					console.log(err);
+				}
+				)
 
-  }
+		}
+	}
 
 
-  function getConvertedTime(time) {
-      let duration = Number(time);
 
-      var hours = Math.floor(duration / 3600);
-      var minutes = Math.floor(duration % 3600 / 60);
+	getConvertedTime(time) {
+		let duration = Number(time);
 
-      return `${hours} hrs ${minutes} min`;
-  }
+		var hours = Math.floor(duration / 3600);
+		var minutes = Math.floor(duration % 3600 / 60);
 
-  $scope.trustSrc = function(src) {
-      return $sce.trustAsResourceUrl(src);
-  };
-  */
+		return `${hours} hrs ${minutes} min`;
+	}
+
+	// trustSrc(src) {
+	// 	return $sce.trustAsResourceUrl(src);
+	// };
 
 }
